5f080a690d6d9effbbd060b17c9be30a2314ae6f
Dan El Chehouri <danelchehouri@gmail.com>
2024-07-11 22:21:00 -0400

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,9 @@
+// src/tasks.js
+const tasks = [
+  { id: 1, name: "Complete the React challenge", isCompleted: false },
+  { id: 2, name: "Review PRs", isCompleted: false },
+  { id: 3, name: "Update documentation", isCompleted: false },
+  { id: 4, name: "Team meeting at 3 PM", isCompleted: false },
+];
+
+export default tasks;
